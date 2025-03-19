486721887bfc283b33e4132280a99e5e38ed2d18
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-03-19 20:08:36 +0100

    Update week_classes_screen.dart

--- esiplanner/lib/screens/week_classes_screen.dart
+++ esiplanner/lib/screens/week_classes_screen.dart
@@ -21,88 +21,95 @@ class WeekClassesScreen extends StatelessWidget {
         title: Text('Clases de la semana ${selectedWeekIndex + 1}'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: _buildEventList(isDarkMode),
+      body: _buildEventList(context),
     );
   }
 
-  Widget _buildEventList(bool isDarkMode) {
+  Widget _buildEventList(BuildContext context) {
     if (events.isEmpty) {
       return Center(
         child: Text(
           'No hay clases esta semana',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: isDarkMode ? Colors.white : Colors.black,
-          ),
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: isDarkMode ? Colors.white : Colors.black,
+              ),
         ),
       );
     }
 
+    final groupedByDate = _groupEventsByDate(events);
+    final sortedDates = groupedByDate.keys.toList()..sort();
+
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      itemCount: sortedDates.length,
+      itemBuilder: (context, index) {
+        final date = sortedDates[index];
+        final events = groupedByDate[date]!..sort(_sortEventsByTime);
+        final isOverlapping = _calculateOverlappingEvents(events);
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0), // Margen inferior de 16.0
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _formatDateToFullDate(DateTime.parse(date)),
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: isDarkMode ? Colors.yellow.shade700 : Colors.indigo,
+                    ),
+              ),
+              ...events.asMap().entries.map((entry) {
+                final index = entry.key;
+                final eventData = entry.value;
+                final event = eventData['event'];
+                final classType = eventData['classType'];
+                final subjectName = eventData['subjectName'];
+
+                return ClassCards(
+                  subjectName: subjectName,
+                  classType: '$classType - ${_getGroupLabel(classType[0])}',
+                  event: event,
+                  isOverlap: isOverlapping[index],
+                );
+              }),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Map<String, List<Map<String, dynamic>>> _groupEventsByDate(List<Map<String, dynamic>> events) {
     final groupedByDate = <String, List<Map<String, dynamic>>>{};
     for (var eventData in events) {
       final eventDate = eventData['event']['date'];
       groupedByDate.putIfAbsent(eventDate, () => []).add(eventData);
     }
+    return groupedByDate;
+  }
 
-    final sortedDates = groupedByDate.keys.toList()..sort();
+  int _sortEventsByTime(Map<String, dynamic> a, Map<String, dynamic> b) {
+    final timeA = DateTime.parse('${a['event']['date']} ${a['event']['start_hour']}');
+    final timeB = DateTime.parse('${b['event']['date']} ${b['event']['start_hour']}');
+    return timeA.compareTo(timeB);
+  }
 
-    return ListView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      itemCount: sortedDates.length,
-      itemBuilder: (context, index) {
-        final date = sortedDates[index];
-        final events = groupedByDate[date]!..sort((a, b) {
-          final timeA = DateTime.parse('${a['event']['date']} ${a['event']['start_hour']}');
-          final timeB = DateTime.parse('${b['event']['date']} ${b['event']['start_hour']}');
-          return timeA.compareTo(timeB);
-        });
+  List<bool> _calculateOverlappingEvents(List<Map<String, dynamic>> events) {
+    final isOverlapping = List<bool>.filled(events.length, false);
+    for (int i = 0; i < events.length - 1; i++) {
+      final endTimeCurrent = DateTime.parse('${events[i]['event']['date']} ${events[i]['event']['end_hour']}');
+      final startTimeNext = DateTime.parse('${events[i + 1]['event']['date']} ${events[i + 1]['event']['start_hour']}');
 
-        final isOverlapping = List<bool>.filled(events.length, false);
-        for (int i = 0; i < events.length - 1; i++) {
-          final endTimeCurrent = DateTime.parse('${events[i]['event']['date']} ${events[i]['event']['end_hour']}');
-          final startTimeNext = DateTime.parse('${events[i + 1]['event']['date']} ${events[i + 1]['event']['start_hour']}');
-
-          if (endTimeCurrent.isAfter(startTimeNext)) {
-            isOverlapping[i] = true;
-            isOverlapping[i + 1] = true;
-          }
-        }
-
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              _formatDateToFullDate(DateTime.parse(date)),
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: isDarkMode ? Colors.yellow.shade700 : Colors.indigo,
-              ),
-            ),
-            ...events.asMap().entries.map((entry) {
-              final index = entry.key;
-              final eventData = entry.value;
-              final event = eventData['event'];
-              final classType = eventData['classType'];
-              final subjectName = eventData['subjectName'];
-
-              return ClassCards(
-                subjectName: subjectName,
-                classType: '$classType - ${_getGroupLabel(classType[0])}',
-                event: event,
-                isOverlap: isOverlapping[index],
-              );
-            }),
-          ],
-        );
-      },
-    );
+      if (endTimeCurrent.isAfter(startTimeNext)) {
+        isOverlapping[i] = true;
+        isOverlapping[i + 1] = true;
+      }
+    }
+    return isOverlapping;
   }
 
   String _formatDateToFullDate(DateTime date) {
