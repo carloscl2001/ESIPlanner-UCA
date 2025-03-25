25c1f9dd045e4a5d9faf823fa991f21fc9de73b7
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-03-25 20:44:31 +0100

    Update api_services.dart

--- esiplanner/lib/services/api_services.dart
+++ esiplanner/lib/services/api_services.dart
@@ -1,7 +1,7 @@
 class ApiServices {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://192.168.1.44:8000',
+    defaultValue: 'http://192.168.1.42:8000',
   ); //usando la ip del pc
   //static const String baseUrl =  String.fromEnvironment('BASE_URL', defaultValue: 'http://10.0.2.2:8000');
 } 
