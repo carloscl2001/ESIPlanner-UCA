fd01b32830c7b8580120e90dd9713af21a521b56
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-04-09 11:18:54 +0200

    Update server.py

--- backend/fastAPI/server.py
+++ backend/fastAPI/server.py
@@ -10,6 +10,7 @@ from routers import auth
 from routers import users
 from routers import degrees
 from routers import subjects
+from fastapi.middleware.cors import CORSMiddleware
 
 # Instanciamos la aplicación
 app = FastAPI()
@@ -20,7 +21,22 @@ app.include_router(users.router)
 app.include_router(degrees.router)
 app.include_router(subjects.router)
 
+# Configuramos CORS (Cross-Origin Resource Sharing)
+# origins = [
+#     "http://localhost",          # Flutter web en desarrollo
+#     "http://localhost:8000",    # Backend local
+#     "http://10.182.119.113",    # Tu IP local
+#     "http://10.182.119.113:8000",
+#     "https://tudominio.com",    # Dominio de producción
+#     "https://app.tudominio.com"
+# ]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En desarrollo permite cualquier origen (en producción usa tu dominio), es decir, usar el vector origins
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
 
 # Definimos una peticion básica
 @app.get("/")
