ebb3a4431e29d39da140ff5c002f34da4294c3be
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-04-17 17:41:10 +0200

    act

--- esiplanner/lib/features/timetable/timetable_principal/timetable_principal_logic.dart
+++ esiplanner/lib/features/timetable/timetable_principal/timetable_principal_logic.dart
@@ -5,7 +5,7 @@ import '../../../services/profile_service.dart';
 import '../../../services/subject_service.dart';
 import '../../../providers/auth_provider.dart';
 
-class TimetablePrincipalLogic with ChangeNotifier {
+class TimetableLogic with ChangeNotifier {
   final BuildContext context;
   final ProfileService _profileService;
   final SubjectService _subjectService;
@@ -19,7 +19,7 @@ class TimetablePrincipalLogic with ChangeNotifier {
   List<DateTimeRange> _weekRanges = [];
   List<String> _weekLabels = [];
 
-  TimetablePrincipalLogic(this.context)
+  TimetableLogic(this.context)
       : _profileService = ProfileService(),
         _subjectService = SubjectService();
 
--- esiplanner/lib/features/timetable/timetable_principal/timetable_principal_widgets_desktop.dart
+++ esiplanner/lib/features/timetable/timetable_principal/timetable_principal_widgets_desktop.dart
@@ -7,43 +7,54 @@ import '../timetable_week/timetable_week_screen.dart';
 class WeekDaysHeaderDesktop extends StatelessWidget {
   final bool isDarkMode;
 
-  const WeekDaysHeaderDesktop({super.key, required this.isDarkMode});
+  WeekDaysHeaderDesktop({super.key, required this.isDarkMode});
 
   @override
   Widget build(BuildContext context) {
+    final accentColor = isDarkMode ? Colors.yellow.shade700 : Colors.indigo;
+    final bgColor = isDarkMode ? Colors.grey[900]! : Colors.grey[50]!;
+    
     return Container(
-      margin: const EdgeInsets.all(8),
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       decoration: BoxDecoration(
-        color: isDarkMode ? Colors.black : Colors.white,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(16.0),
-          bottomRight: Radius.circular(16.0),
-        ),
-        border: Border.all(
-          color: isDarkMode ? Colors.yellow.shade700 : Colors.indigo,
-          width: 4,
-        ),
+        color: bgColor,
+        borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-            color: isDarkMode ? Colors.grey.withAlpha(115) : Colors.black.withAlpha(115),
-            blurRadius: 8.0,
-            offset: const Offset(0, 3),
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 2,
           ),
         ],
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: ['Lun', 'Mar', 'Mie', 'Jue', 'Vie'].map((day) {
-            return SizedBox(
-              child: Center(
-                child: Text(
-                  day,
-                  style: TextStyle(
-                    color: isDarkMode ? Colors.yellow.shade700 : Colors.indigo,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
+          children: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'].map((day) {
+            return Expanded(
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: accentColor,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: Text(
+                      day,
+                      style: TextStyle(
+                        color: accentColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -56,7 +67,7 @@ class WeekDaysHeaderDesktop extends StatelessWidget {
 }
 
 class WeekSelectorDesktop extends StatelessWidget {
-  final TimetablePrincipalLogic timetableLogic;
+  final TimetableLogic timetableLogic;
   final bool isDarkMode;
 
   const WeekSelectorDesktop({
@@ -73,10 +84,8 @@ class WeekSelectorDesktop extends StatelessWidget {
     return ListView.builder(
       key: const PageStorageKey('timetable'),
       shrinkWrap: false,
-      physics: defaultTargetPlatform == TargetPlatform.iOS
-          ? const BouncingScrollPhysics()
-          : const ClampingScrollPhysics(),
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       itemCount: weeks.length,
       itemBuilder: (context, index) {
         final weekDays = weeks[index];
@@ -84,7 +93,6 @@ class WeekSelectorDesktop extends StatelessWidget {
         final isCurrentWeek = index == currentWeekIndex;
 
         return Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             if (index == 0 || timetableLogic.isNewMonth(weekDays, weeks[index - 1]))
               _buildMonthHeader(startDate, isDarkMode),
@@ -102,38 +110,28 @@ class WeekSelectorDesktop extends StatelessWidget {
   }
 
   Widget _buildMonthHeader(DateTime startDate, bool isDarkMode) {
+    final bgColor = isDarkMode ? Colors.grey : Colors.grey;
+    
     return Padding(
       padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: isDarkMode ? Colors.grey.shade800 : Colors.grey.shade600,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Text(
-              DateFormat('MMMM', 'es_ES').format(startDate),
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: isDarkMode ? Colors.white : Colors.white,
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: bgColor,
+                borderRadius: BorderRadius.circular(8),
               ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: isDarkMode ? Colors.grey.shade800 : Colors.grey.shade600,
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Text(
-              DateFormat('y', 'es_ES').format(startDate),
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: isDarkMode ? Colors.white : Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Center(
+                child: Text(
+                  '${DateFormat('MMMM', 'es_ES').format(startDate)}    ${DateFormat('y').format(startDate)}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ),
@@ -148,7 +146,7 @@ class WeekRowDesktop extends StatelessWidget {
   final int weekIndex;
   final bool isDarkMode;
   final bool isCurrentWeek;
-  final TimetablePrincipalLogic timetableLogic;
+  final TimetableLogic timetableLogic;
 
   const WeekRowDesktop({
     super.key,
@@ -161,78 +159,84 @@ class WeekRowDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final accentColor = isDarkMode ? Colors.yellow.shade700 : Colors.indigo;
+    final bgColor = isDarkMode ? Colors.grey[850]! : Colors.white;
+    final textColor = isDarkMode ? Colors.white : Colors.black;
+    
     return GestureDetector(
-      onTap: () {
-        final weekRange = timetableLogic.weekRanges[weekIndex];
-        final allEvents = timetableLogic.getFilteredEvents(weekRange);
-
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => TimetableWeekScreen(
-              events: allEvents,
-              selectedWeekIndex: weekIndex,
-              isDarkMode: isDarkMode,
-              weekStartDate: weekDays.first,
-            ),
-          ),
-        );
-      },
+      onTap: () => _navigateToWeekScreen(context),
       child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8.0),
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        margin: const EdgeInsets.symmetric(vertical: 6),
         decoration: BoxDecoration(
-          color: isDarkMode ? Colors.black : Colors.white,
-          borderRadius: BorderRadius.circular(20),
+          color: bgColor,
+          borderRadius: BorderRadius.circular(10),
           border: isCurrentWeek
-              ? Border.all(
-                  color: isDarkMode ? Colors.yellow.shade700 : Colors.indigo,
-                  width: 4,
-                )
+              ? Border.all(color: accentColor, width: 2)
               : null,
           boxShadow: [
             BoxShadow(
-              color: !isDarkMode
-                  ? Colors.black.withAlpha(115)
-                  : Colors.grey.withAlpha(115),
-              blurRadius: 12.0,
-              offset: const Offset(0, 0),
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 6,
+              offset: const Offset(0, 2),
             ),
           ],
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: weekDays.map((day) {
-            final hasClass = timetableLogic.dayHasClass(day);
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: weekDays.map((day) {
+              final hasClass = timetableLogic.dayHasClass(day);
+              
+              return _buildDayCell(day, hasClass, textColor, accentColor);
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
 
-            return SizedBox(
-              width: 60,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    DateFormat('d', 'es_ES').format(day),
-                    style: TextStyle(
-                      color: isDarkMode ? Colors.white : Colors.black,
-                      fontSize: 32,
-                    ),
-                  ),
-                  if (hasClass)
-                    Positioned(
-                      bottom: 4,
-                      child: Container(
-                        width: 8,
-                        height: 8,
-                        decoration: BoxDecoration(
-                          color: isDarkMode ? Colors.white : Colors.black,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ),
-                ],
+  Widget _buildDayCell(DateTime day, bool hasClass, Color textColor, Color accentColor) {
+    return SizedBox(
+      width: 72,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            DateFormat('d').format(day),
+            style: TextStyle(
+              color: textColor,
+              fontSize: 28,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 4),
+          if (hasClass)
+            Container(
+              width: 8,
+              height: 8,
+              decoration: BoxDecoration(
+                color: accentColor,
+                shape: BoxShape.circle,
               ),
-            );
-          }).toList(),
+            ),
+        ],
+      ),
+    );
+  }
+
+  void _navigateToWeekScreen(BuildContext context) {
+    final weekRange = timetableLogic.weekRanges[weekIndex];
+    final allEvents = timetableLogic.getFilteredEvents(weekRange);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TimetableWeekScreen(
+          events: allEvents,
+          selectedWeekIndex: weekIndex,
+          isDarkMode: isDarkMode,
+          weekStartDate: weekDays.first,
         ),
       ),
     );
@@ -244,49 +248,55 @@ class BuildEmptyCardDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final textColor = isDarkMode ? Colors.white70 : Colors.black54;
+    
     return Center(
-      child: Card(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 600),
         margin: const EdgeInsets.all(24),
-        elevation: 4,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.person,
-                    size: 96,
-                    color: Theme.of(context).disabledColor,
-                  ),
-                  Icon(
-                    Icons.arrow_right_rounded, 
-                    size: 96, 
-                    color: Theme.of(context).disabledColor,
-                  ),
-                  Icon(
-                    Icons.edit_note_rounded,
-                    size: 96,
-                    color: Theme.of(context).disabledColor,
-                  ),
-                ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.school_outlined,
+              size: 120,
+              color: textColor,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Planifica tu horario',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+                color: textColor,
               ),
-              const SizedBox(height: 24),
-              Text(
-                'Selecciona asignaturas en perfil',
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                  color: Theme.of(context).disabledColor,
-                  fontSize: 24,
-                ),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                'Selecciona tus asignaturas en la sección de perfil para comenzar a visualizar tu horario semanal',
                 textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: textColor,
+                  height: 1.5,
+                ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 32),
+            FilledButton.icon(
+              onPressed: () {
+                // Navegar a la sección de perfil
+              },
+              icon: const Icon(Icons.person),
+              label: const Text('Ir a Perfil'),
+              style: FilledButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              ),
+            ),
+          ],
         ),
       ),
     );
