d5b917e0987af2962df8bbef5ec2b4729070163c
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-04-21 12:19:07 +0200

    Update api_services.dart

--- esiplanner/lib/services/api_services.dart
+++ esiplanner/lib/services/api_services.dart
@@ -1,8 +1,8 @@
 //PARA SERVIDOR EN LOCAL
 class ApiServices {
 
-  static const String _devBaseUrlMovil = 'http://192.168.1.46:8000'; // Desarrollo local en movil fisico
-  // static const String _devBaseUrlWeb = 'http://localhost:8000'; // Para navegador
+  // static const String _devBaseUrlMovil = 'http://10.182.119.113:8000'; // Desarrollo local en movil fisico
+  static const String _devBaseUrlWeb = 'http://localhost:8000'; // Para navegador
   // static const String _prodBaseUrl = 'https://api.tudominio.com'; // Producción
   // static const String _emuladorBaseUrl = 'http://10.0.2.2:8000'; // Emulador de Android
   // static const String _railwayBaseUrl = 'https://servidor-production-42cc.up.railway.app'; // Railway
@@ -10,7 +10,7 @@ class ApiServices {
   //usando la ip del pc
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: _devBaseUrlMovil,
+    defaultValue: _devBaseUrlWeb,
   ); 
 
 } 
