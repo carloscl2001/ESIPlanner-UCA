d695ef14d5ec9181e991e9b56cf38c204a4d112e
carloscl2001 <carlosantonio.corteslora@alum.uca.es>
2025-04-30 09:56:45 +0200

    Update api_services.dart

--- esiplanner/lib/services/api_services.dart
+++ esiplanner/lib/services/api_services.dart
@@ -1,9 +1,9 @@
 //PARA SERVIDOR EN LOCAL
 class ApiServices {
 
-  // static const String _devBaseUrl = 'http://10.182.119.113:8000'; // Desarrollo local en movil fisico UCA
+  static const String _devBaseUrl = 'http://10.182.119.113:8000'; // Desarrollo local en movil fisico UCA
   // static const String _devBaseUrl = 'http://192.168.1.37:8000'; // Desarrollo local en movil fisico CASA
-  static const String _devBaseUrl = 'http://localhost:8000'; // Para navegador
+  // static const String _devBaseUrl = 'http://localhost:8000'; // Para navegador
   // static const String _devBaseUrl = 'https://api.tudominio.com'; // Producción
   // static const String _devBaseUrl = 'http://10.0.2.2:8000'; // Emulador de Android
   // static const String _devBaseUrl = 'https://servidor-production-42cc.up.railway.app'; // Railway
